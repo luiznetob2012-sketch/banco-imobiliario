787e3db68f0dfb8ed2400bff98bce4be88c8a66b
luiznetob2012-sketch <luiznetob2012@gmail.com>
2026-09-18 21:17:35 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE='banco-super-imobiliario-v1';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate',event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch',event=>{
+  const request=event.request;
+  if(request.method!=='GET'||new URL(request.url).origin!==self.location.origin)return;
+  event.respondWith(caches.match(request).then(cached=>cached||fetch(request).then(response=>{
+    if(response.ok){const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(request,copy));}
+    return response;
+  }).catch(()=>request.mode==='navigate'?caches.match('./index.html'):Response.error())));
+});
